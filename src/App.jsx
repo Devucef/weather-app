3a8e3d794e1c5101407b25ee438a488c8d460249
Youssef Elf <devucef01@gmail.com>
2025-07-05 09:29:38 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Provider } from "react-redux";
 import { store } from "@/app/store/store";
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider } from "@/context/ThemeContext";
 import Start from "@/components/blocs/Start";
 import ModalContainer from "@/components/UI/Modal/ModalContainer";
 import Container from "@/components/blocs/Container";
